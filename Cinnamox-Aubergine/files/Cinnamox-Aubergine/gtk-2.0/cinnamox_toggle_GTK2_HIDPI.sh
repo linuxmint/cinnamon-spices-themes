4520d30cf1179cbe08267390c515ef88c84ae825
smurphos <29017677+smurphos@users.noreply.github.com>
2018-03-04 09:54:16 +0000

    Cinnamox themes - 20180304 update

---
+++ Cinnamox-Aubergine/files/Cinnamox-Aubergine/gtk-2.0/cinnamox_toggle_GTK2_HIDPI.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Description: Helper script to toggle GTK2 HIDPI Support
+THEMENAME=Cinnamox-Aubergine
+if [ ! -f "$PWD/gtkrc" ]; then
+	echo "Something is wrong. Cannot find $PWD/gtkrc"
+    echo "";
+    read -p "Press enter to exit the script.";
+    cd;
+    exit 1;
+fi
+if [ ! -f "gtkrc.nohidpi" ]; then
+    cp gtkrc gtkrc.nohidpi && cp -f gtkrc.hidpi gtkrc && echo "Enabled HIDPI in GTK2. Reloading $THEMENAME.";
+    gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y";
+	gsettings set org.cinnamon.desktop.interface gtk-theme "$THEMENAME";
+else
+	cp -f gtkrc.nohidpi gtkrc && rm gtkrc.nohidpi && echo "Disabled HIDPI in GTK2. Reloading $THEMENAME.";
+	gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y";
+	gsettings set org.cinnamon.desktop.interface gtk-theme "$THEMENAME";
+fi
+    echo "";
+    read -p "Press enter to exit the script.";
+    cd;
+    exit;
