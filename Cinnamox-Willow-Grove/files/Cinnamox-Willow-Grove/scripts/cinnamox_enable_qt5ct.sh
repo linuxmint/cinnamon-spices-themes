9e2754fa009e6075c608dacf808263a560e794e8
Steve M <29017677+smurphos@users.noreply.github.com>
2020-05-24 08:40:13 +0100

    Cinnamox Theme updates 240520 (#637)

---
+++ Cinnamox-Willow-Grove/files/Cinnamox-Willow-Grove/scripts/cinnamox_enable_qt5ct.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Description: Helper file to move Cinnamox-Willow-Grove.conf to /$HOME/.config/qt5ct/colors folder
+THEMENAME=Cinnamox-Willow-Grove;
+DIRECTORY="/home/$USER/.themes/$THEMENAME/qt5ct/";
+QTDIR="$HOME/.config/qt5ct/colors";
+cd "$DIRECTORY" || exit;
+if [ ! -f "$PWD/$THEMENAME.conf" ]; then
+    if [ -t 1 ]; then
+        echo "Something is wrong. Cannot find $PWD/$THEMENAME.conf"
+        echo "";
+        read -rp "Press enter to exit the script.";
+    fi
+    exit 1;
+fi
+if [ ! -d "$QTDIR" ]; then
+    mkdir "$QTDIR";
+fi
+echo "Copying $PWD/$THEMENAME.conf to $QTDIR/$THEMENAME.conf"
+cp "$PWD/$THEMENAME.conf" "$QTDIR/$THEMENAME.conf";
+if [ -t 1 ]; then
+    echo "";
+    read -rp "Press enter to exit the script.";
+fi
+exit;
---
+++ Cinnamox-Willow-Grove/files/Cinnamox-Willow-Grove/scripts/cinnamox_toggle_cinnamon.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Description: Helper script to toggle different versions of cinnamon.css
+THEMENAME=Cinnamox-Willow-Grove;
+DIRECTORY="/home/$USER/.themes/$THEMENAME/cinnamon/";
+cd "$DIRECTORY" || exit;
+if [ ! -f "$PWD/cinnamon.css" ]; then
+    echo "Something is wrong. Cannot find $PWD/cinnamon.css"
+    echo "";
+    read -rp "Press enter to exit the script.";
+    exit 1;
+fi
+if [ ! -f "cinnamon_new.css" ]; then
+    cp cinnamon.css cinnamon_new.css && cp -f cinnamon_old.css cinnamon.css && echo "Enabled Cinnamon old style. Reloading $THEMENAME.";
+    gsettings reset org.cinnamon.theme name;
+    gsettings set org.cinnamon.theme name "$THEMENAME";
+else
+    cp -f cinnamon_new.css cinnamon.css && rm cinnamon_new.css && echo "Enabled Cinnamon new style. Reloading $THEMENAME.";
+    gsettings reset org.cinnamon.theme name;
+    gsettings set org.cinnamon.theme name "$THEMENAME";
+fi
+if [ -t 1 ]; then
+    echo "";
+    read -rp "Press enter to exit the script.";
+fi
+exit;
